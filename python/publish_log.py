b2d22b0289129da74a5eec031ecc6363b08d66f2
levymp <levymp@umich.edu>
2020-10-08 12:47:46 -0400

    updating publsih

--- python/publish_log.py
+++ python/publish_log.py
@@ -5,39 +5,40 @@ import signal
 import os
 
 
-# start the timesync
-p_time = subprocess.Popen(['timesync'], cwd=os.path.abspath("../bin"))
-HOST = ""
-COMMAND = 
-# start mobile bot
-p_mobilebot = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
+# # start the timesync
+# p_time = subprocess.Popen(['timesync'], cwd=os.path.abspath("../bin"))
+# HOST = ""
+# COMMAND = 
+# # start mobile bot
+# p_mobilebot = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
 
-)
-
-
-# start the logger
-p_logger = subprocess.Popen(['lcm-logger', "../static/log_test", "-i", "-q"])
-time.sleep(20)
-
-# stop the log file
-p_logger.send_signal(signal.SIGINT)
-
-# stop time 
-p_time.send_signal(signal.SIGINT)
+# )
 
 
+# # start the logger
+# p_logger = subprocess.Popen(['lcm-logger', "../static/log_test", "-i", "-q"])
+# time.sleep(20)
 
-# def post_log(file_name):
-#     file = open('../static/first_log.log', 'rb')
-#     # print(file)
-#     payload = {'logfile': file}
-#     url = 'http://127.0.0.1:8505/MBOT/v1/api/'
-#     # r = requests.get('https://ip.picotrillion.com/api/geo/v1/ip')
+# # stop the log file
+# p_logger.send_signal(signal.SIGINT)
 
-#     r = requests.post(url, files=payload)
+# # stop time 
+# p_time.send_signal(signal.SIGINT)
 
-#     print(r.status_code)
-#     print(r.json())
-#     print(r.status_code)
-#     # val = r.json()
-#     file.close()
+
+
+def post_log(file_name):
+    file = open('../static/' + file_name, 'rb')
+    payload = {'logfile': file}
+    url = 'https://api.mplevy.com/api/mbot/v1/log'
+
+    r = requests.post(url, files=payload)
+
+    print(r.status_code)
+    print(r.json())
+    
+    file.close()
+
+
+if __name__ == '__main__':
+    post_log('first_log.log')
