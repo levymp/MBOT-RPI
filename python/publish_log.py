4ae5eb58b0e3f4399f53d89b036634f1d0cbe910
levymp <levymp@umich.edu>
2020-10-10 19:29:21 -0400

    updating publish log

--- python/publish_log.py
+++ python/publish_log.py
@@ -4,41 +4,56 @@ import time
 import signal
 import os
 
-
-# # start the timesync
-# p_time = subprocess.Popen(['timesync'], cwd=os.path.abspath("../bin"))
-# HOST = ""
-# COMMAND = 
-# # start mobile bot
-# p_mobilebot = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
-
-# )
-
-
-# # start the logger
-# p_logger = subprocess.Popen(['lcm-logger', "../static/log_test", "-i", "-q"])
-# time.sleep(20)
-
-# # stop the log file
-# p_logger.send_signal(signal.SIGINT)
-
-# # stop time 
-# p_time.send_signal(signal.SIGINT)
+_BASE_URL = 'https://api.mplevy.com/api/mbot/v1/'
+_LOG_URL = _BASE_URL + 'log'
 
 
 
-def post_log(file_name):
-    file = open('../static/' + file_name, 'rb')
-    payload = {'logfile': file}
-    url = 'https://api.mplevy.com/api/mbot/v1/log'
 
-    r = requests.post(url, files=payload)
-
-    print(r.status_code)
-    print(r.json())
+def post_log(botname, description, path):
+    '''pass in path string'''
+    # read file
+    file_path = Path(path)
     
+    # check if correct path
+    if not file_path.is_file():
+        return -1
+
+
+    # open file
+    file = open(file_path, 'rb')
+
+    # create payload
+    payload = {'logfile': file}
+    param = {'name': botname, 'description': description}
+    # post file
+    r = requests.post(_LOG_URL, params=param, files=payload)
+    
+    # close file
     file.close()
+    if r.status_code != 200: 
+        print(r.text)
+        return -1
+    else: 
+        return r.json()
+
+
+def main():
+    # start the timesync
+    p_time = subprocess.Popen(['timesync'], cwd=os.path.abspath("../bin"))
+
+    # start the logger
+    p_logger = subprocess.Popen(['lcm-logger', "../static/log_test", "-i", "-q"])
+    
+
+    # stop the log file
+    p_logger.send_signal(signal.SIGINT)
+
+    # stop time 
+    p_time.send_signal(signal.SIGINT)
+
+
 
 
 if __name__ == '__main__':
-    post_log('first_log.log')
+    main()
