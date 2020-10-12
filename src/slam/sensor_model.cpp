33bd2a4800ec0fc8384e61966931cfce84710070
sansaldo <sam@ansaldo.net>
2020-10-12 17:50:52 -0400

    Testing pos estimates

--- src/slam/sensor_model.cpp
+++ src/slam/sensor_model.cpp
@@ -18,7 +18,7 @@ double SensorModel::likelihood(const particle_t& sample, const lidar_t& scan, co
 	MovingLaserScan mvscan(scan, sample.parent_pose, sample.pose);
 
 	for(const auto& ray : mvscan){
-		float rayscore = .0001;
+		float rayscore = .001;
         Point<float> rayE;
         rayE.x = ray.range * std::cos(ray.theta) + ray.origin.x;
         rayE.y = ray.range * std::sin(ray.theta)+ ray.origin.y;
@@ -50,7 +50,7 @@ double SensorModel::likelihood(const particle_t& sample, const lidar_t& scan, co
             while(x != rayEndCell.x || y != rayEndCell.y){
                 if(map.isCellInGrid(x, y)) {
                     if(lods < 10){
-						rayscore += .01;
+						rayscore += .001;
 					}
                 }
                 int e2 = 2 * err;
