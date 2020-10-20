6778dc90a1e91b11b7b23219e71c42c4138da820
sansaldo <sam@ansaldo.net>
2020-10-20 14:22:36 -0400

    Added testing motion fixes

--- src/mbot/motion_controller_new.cpp
+++ src/mbot/motion_controller_new.cpp
@@ -83,7 +83,7 @@ public:
         
         
         const float kd = 0.75f; // distance coefficients
-        const float kb = -0.2f; // goal pose coefficient
+        const float kb = -0.5f; // goal pose coefficient
         const float ka = 1.75*(2.0/3.14*kd - 1.66*kb); // heading coefficient
         
         mbot_motor_command_t cmd;
