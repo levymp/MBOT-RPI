c5c7068ad7f73bcd610dc345b95fde4442fbb3ec
sansaldo <sam@ansaldo.net>
2020-10-20 15:19:20 -0400

    Added testing motion fixes

--- src/mbot/motion_controller_new.cpp
+++ src/mbot/motion_controller_new.cpp
@@ -214,7 +214,7 @@ private:
 
     bool haveReachedTarget(void)
     {
-        const float kPosTolerance = 0.05f;
+        const float kPosTolerance = 0.1f;
 	    const float kFinalPosTolerance = 0.025f;
 
         //tolerance for intermediate waypoints can be more lenient
@@ -238,7 +238,7 @@ private:
         float yError = std::abs(target.y - pose.y);
         float thetaError = std::abs(target.theta - pose.theta);
 
-        return (xError < tolerance) && (yError < tolerance) && (thetaError < 0.1);
+        return (xError < tolerance) && (yError < tolerance) && (thetaError < (tolerance/10));
     }
     
     bool assignNextTarget(void)
