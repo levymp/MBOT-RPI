3a251ff1e320c765630ec19302a482ddcfc6eea3
sansaldo <sam@ansaldo.net>
2020-10-22 16:12:37 -0400

    Added Function to init map to filter

---
+++ src/mbot/comp_3.cpp
@@ -0,0 +1,25 @@
+#include <common/lcm_config.h>
+#include <mbot/mbot_channels.h>
+#include <lcmtypes/robot_path_t.hpp>
+#include <lcm/lcm-cpp.hpp>
+#include <iostream>
+#include <unistd.h>
+#include <cmath>
+
+int main(int argc, char** argv)
+{
+	mbot_motor_command_t cmd;
+
+    cmd.trans_v = 0.0f;
+    cmd.angular_v = .2f;
+    lcmInstance.publish(MBOT_MOTOR_COMMAND_CHANNEL, &cmd);
+
+    sleep(1);
+
+    cmd.trans_v = 0.0f;
+    cmd.angular_v = 0.0f;
+
+	lcmInstance.publish(MBOT_MOTOR_COMMAND_CHANNEL, &cmd);
+
+    return 0;
+}
