dbd5e52498e2f851021b946384bf5e2463461a50
sansaldo <sam@ansaldo.net>
2020-10-24 16:31:45 -0400

    changes on bot

--- launch_botlab.sh
+++ launch_botlab.sh
@@ -2,6 +2,5 @@
 source setenv.sh
 ./bin/timesync &> /dev/null &
 ./bin/rplidar_driver &> /dev/null &
-./bin/slam &> /dev/null &
-./bin/motion_controller &> /dev/null &
+./bin/motion_controller_new &> /dev/null &
 ./bin/botgui
