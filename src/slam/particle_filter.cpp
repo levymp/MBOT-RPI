1ae3ff0199c8093d7831a61b35f396e387045cb2
sansaldo <sam@ansaldo.net>
2020-10-24 15:00:39 -0400

    Added Function to init map to filter

--- src/slam/particle_filter.cpp
+++ src/slam/particle_filter.cpp
@@ -81,6 +81,7 @@ void ParticleFilter::addNoise(const OccupancyGrid& map)
     std::uniform_int_distribution<int> dist(0, emptyCells.size());
     std::uniform_int_distribution<int> rot(-3.14, 3.14);
     int scatter = int((30000 - mapscore)/10000);
+    printf("scatter: %f\n", scatter);
 
     for(int i = 0; i<posterior_.size(); i++){
         if(i%scatter == 0){
