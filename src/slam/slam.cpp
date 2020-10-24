459bb6834a5fb06d7d3dfa4116d60d24dd8ed1a7
sansaldo <sam@ansaldo.net>
2020-10-24 14:15:33 -0400

    Added Function to init map to filter

--- src/slam/slam.cpp
+++ src/slam/slam.cpp
@@ -313,7 +313,7 @@ void OccupancyGridSLAM::exploreRandom(void)
 
     bool fwdflg = true;
     for(const auto& ray : mvscan){
-        if(ray.theta > -.025 || ray.theta < .025){
+        if(ray.theta > -.01 || ray.theta < .01){
             if(ray.range < .2f){
                 fwdflg = false;
             }
