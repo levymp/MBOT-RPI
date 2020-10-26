8531378ecd24738e4e25622ef8cde8d69d89d26b
sansaldo <sam@ansaldo.net>
2020-10-26 13:06:35 -0400

    merged test-a-star

--- src/planning/motion_planner.cpp
+++ src/planning/motion_planner.cpp
@@ -69,13 +69,8 @@ bool MotionPlanner::isValidGoal(const pose_xyt_t& goal) const
         // And is far enough from obstacles that the robot can physically occupy the space
         // Add an extra cell to account for discretization error and make motion a little safer by not trying to
         // completely snuggle up against the walls in the motion plan
-<<<<<<< HEAD
 
         return distances_(goalCell.x, goalCell.y) > .075;
-=======
-        // std::cout << "distances_(" << goalCell.x << " , " << goalCell.y << ") = " << distances_(goalCell.x, goalCell.y) << std::endl;
-        return distances_(goalCell.x, goalCell.y) > .2;
->>>>>>> test-a-star
     }
     
     // A goal must be in the map for the robot to reach it
