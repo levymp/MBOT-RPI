36bc862525b4cafb6af14ac6abcdcfe0ae9a689d
sansaldo <sam@ansaldo.net>
2020-10-25 23:27:45 -0400

    Added sctter to slam

--- src/slam/slam.cpp
+++ src/slam/slam.cpp
@@ -279,7 +279,7 @@ void OccupancyGridSLAM::updateLocalization(void)
 
             if(mode_ == localization_only && goHome < 10){
                 exploreRandom();
-                if(scatter < 400){
+                if(scatter < 300){
                     goHome++;
                 }
             }
