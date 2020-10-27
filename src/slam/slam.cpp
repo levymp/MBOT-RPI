356cd0a2b0f13995f8304e42659ab7fdd6428b1e
sansaldo <sam@ansaldo.net>
2020-10-27 11:06:13 -0400

    Messed with local parameters

--- src/slam/slam.cpp
+++ src/slam/slam.cpp
@@ -277,7 +277,7 @@ void OccupancyGridSLAM::updateLocalization(void)
 
             if(mode_ == localization_only && goHome < 10){
                 exploreRandom();
-                if(scatter > 700){
+                if(scatter > 400){
                     goHome++;
                 }
             }
